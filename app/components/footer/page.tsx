0dfb1658ea410c2c2887d76e68b9762b2990eaf4
Rahul <rahulgupta.css@gmail.com>
2026-08-05 12:37:10 +0530

    update pages

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import {
     FaFacebookF as Facebook,
     FaLinkedinIn as Linkedin,
@@ -25,7 +24,7 @@ const links = [
     "Contact",
 ];
 
-const containerVariants = {
+const containerVariants: Variants = {
     hidden: { opacity: 0 },
     show: {
         opacity: 1,
@@ -35,7 +34,7 @@ const containerVariants = {
     },
 };
 
-const itemVariants = {
+const itemVariants: Variants = {
     hidden: { opacity: 0, y: 30 },
     show: {
         opacity: 1,
